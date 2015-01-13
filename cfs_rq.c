1a4fa687e579a72a8cc43abcfb9f5767e0769739
Tony <suokun24@126.com>
2015-01-13 16:28:47 -0700

    Create cfs_rq.c

---
+++ cfs_rq.c
@@ -0,0 +1,89 @@
+# /kernel/sched/sched.h
+
+
+/* CFS-related fields in a runqueue */
+struct cfs_rq {
+        struct load_weight load;
+        unsigned int nr_running, h_nr_running;
+
+        u64 exec_clock;
+        u64 min_vruntime;
+#ifndef CONFIG_64BIT
+        u64 min_vruntime_copy;
+#endif
+
+        struct rb_root tasks_timeline;
+        struct rb_node *rb_leftmost;
+
+        /*
+        |* 'curr' points to currently running entity on this cfs_rq.
+        |* It is set to NULL otherwise (i.e when none are currently running).
+        |*/
+        struct sched_entity *curr, *next, *last, *skip;
+
+#ifdef  CONFIG_SCHED_DEBUG
+        unsigned int nr_spread_over;
+#endif
+
+#ifdef CONFIG_SMP
+        /*
+        |* CFS Load tracking
+        |* Under CFS, load is tracked on a per-entity basis and aggregated up.
+        |* This allows for the description of both thread and group usage (in
+        |* the FAIR_GROUP_SCHED case).
+        |*/
+        unsigned long runnable_load_avg, blocked_load_avg;
+        atomic64_t decay_counter;
+        u64 last_decay;
+        atomic_long_t removed_load;
+
+#ifdef CONFIG_FAIR_GROUP_SCHED
+        /* Required to track per-cpu representation of a task_group */
+        u32 tg_runnable_contrib;
+        unsigned long tg_load_contrib;
+
+        /*
+        |*   h_load = weight * f(tg)
+        |*
+        |* Where f(tg) is the recursive weight fraction assigned to
+        |* this group.
+        |*/
+        unsigned long h_load;
+        u64 last_h_load_update;
+        struct sched_entity *h_load_next;
+#endif /* CONFIG_FAIR_GROUP_SCHED */
+#endif /* CONFIG_SMP */
+
+#ifdef CONFIG_FAIR_GROUP_SCHED
+        struct rq *rq;  /* cpu runqueue to which this cfs_rq is attached */
+
+        /*
+        |* leaf cfs_rqs are those that hold tasks (lowest schedulable entity in
+        |* a hierarchy). Non-leaf lrqs hold other higher schedulable entities
+        |* (like users, containers etc.)
+        |*
+        |* leaf_cfs_rq_list ties together list of leaf cfs_rq's in a cpu. This
+        |* list is used during load balance.
+        |*/
+        int on_list;
+        struct list_head leaf_cfs_rq_list;
+        struct task_group *tg;  /* group that "owns" this runqueue */
+
+#ifdef CONFIG_CFS_BANDWIDTH
+        int runtime_enabled;
+        u64 runtime_expires;
+        s64 runtime_remaining;
+
+        u64 throttled_clock, throttled_clock_task;
+        u64 throttled_clock_task_time;
+        int throttled, throttle_count;
+        struct list_head throttled_list;
+#endif /* CONFIG_CFS_BANDWIDTH */
+#endif /* CONFIG_FAIR_GROUP_SCHED */
+};
+
+
+
+
+
+
